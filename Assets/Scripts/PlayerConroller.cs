3c32b529e09ab831fc73b48b920f5978a2a5a1a3
EvaLConway <evalconway@gmail.com>
2016-09-08 11:25:01 -0700

    Walls and Camera

--- Assets/Scripts/PlayerConroller.cs
+++ Assets/Scripts/PlayerConroller.cs
@@ -3,8 +3,22 @@ using System.Collections;
 
 public class PlayerConroller : MonoBehaviour {
 
+	public float speed;
+
+	private Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+
+	}
 	void FixedUpdate ()
 	{
-		Input
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement);
 	}
 }
