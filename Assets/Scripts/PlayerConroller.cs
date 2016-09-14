023691730f1176ff705e3d39400f87d8024eba2c
EvaLConway <evalconway@gmail.com>
2016-09-14 11:21:19 -0700

    Finished "Roll a Ball" Game

--- Assets/Scripts/PlayerConroller.cs
+++ Assets/Scripts/PlayerConroller.cs
@@ -1,17 +1,24 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerConroller : MonoBehaviour {
 
 	public float speed;
+	public Text countText;
+	public Text winText;
 
 	private Rigidbody rb;
+	private int count;
 
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody> ();
-
+		count = 0;
+		SetCountText ();
+		winText.text = "";
 	}
+
 	void FixedUpdate ()
 	{
 		float moveHorizontal = Input.GetAxis ("Horizontal");
@@ -27,6 +34,17 @@ public class PlayerConroller : MonoBehaviour {
 		if (other.gameObject.CompareTag ("Pick Up")) 
 		{
 			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText ()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 8)
+		{
+			winText.text = "You Win!";
 		}
 	}
 }
